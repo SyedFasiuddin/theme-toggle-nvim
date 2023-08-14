439ec0a82001a47e656a72835c8f0e1684f51e3c
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-08-14 22:59:23 +0530

    implementation complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use winit::event::Event;
+use winit::event::WindowEvent;
+use winit::event_loop::EventLoop;
+use winit::window::Theme;
+use winit::window::WindowBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_visible(false)
+        .build(&event_loop)
+        .unwrap();
+
+    if let Some(theme) = window.theme() {
+        match theme {
+            Theme::Light => println!("light"),
+            Theme::Dark => println!("dark"),
+        }
+    } else {
+        println!("dark");
+    }
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::ThemeChanged(theme),
+                ..
+            } => match theme {
+                Theme::Light => println!("light"),
+                Theme::Dark => println!("dark"),
+            },
+            _ => (),
+        }
+    });
 }
